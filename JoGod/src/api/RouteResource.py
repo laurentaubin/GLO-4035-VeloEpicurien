8c50d1802c450324ba19ee90122e69989e504ac9
Toma Gagne <toma.gagne.1@ulaval.ca>
2021-11-30 19:46:57 -0500

    route

--- JoGod/src/api/RouteResource.py
+++ JoGod/src/api/RouteResource.py
@@ -1,6 +1,21 @@
+from typing import List
+
+from flask import request
 from service.RouteService import RouteService
 
 
 class RouteResource:
     def __init__(self, route_service: RouteService):
         self.__route_service = route_service
+
+    def generate_route(self, request):
+        body = request.get_json()
+        if not body:
+            return {"error": "bad request", "description": "missing body"}
+
+        starting_point: dict = body["startingPoint"]
+        type: List[str] = body["type"]
+        number_of_stops: int = body["numberOfStops"]
+
+        self.__route_service.generate_route()
+
--- JoGod/src/service/RouteService.py
+++ JoGod/src/service/RouteService.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from domain.graph.GraphRepository import GraphRepository
 from domain.restaurant.RestaurantRepository import RestaurantRepository
 
@@ -6,3 +8,6 @@ class RouteService:
     def __init__(self, restaurant_repository: RestaurantRepository, graph_repository: GraphRepository):
         self.__restaurant_repository = restaurant_repository
         self.__graph_repository = graph_repository
+
+    def generate_route(self, starting_point: dict, type: List[str], number_of_stops: int, length: int):
+        pass
