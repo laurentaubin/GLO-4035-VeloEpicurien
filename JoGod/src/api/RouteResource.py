7244c70ddfa9ed72d617e524ec52cd20a5f5ab34
laurentaubin <44675437+laurentaubin@users.noreply.github.com>
2021-12-04 20:44:38 -0500

    Add validation to parcours route (#14)

--- JoGod/src/api/RouteResource.py
+++ JoGod/src/api/RouteResource.py
@@ -2,13 +2,17 @@ from typing import List
 
 from flask import request, make_response
 from service.RouteService import RouteService
+from domain.restaurant.RestaurantRepository import RestaurantRepository
 
 from jsonschema import validate
 
 
 class RouteResource:
-    def __init__(self, route_service: RouteService):
+    def __init__(
+        self, route_service: RouteService, restaurant_repository: RestaurantRepository
+    ):
         self.__route_service = route_service
+        self.__restaurant_repository = restaurant_repository
 
     def find_starting_point(self, request):
         body = request.get_json()
@@ -42,8 +46,17 @@ class RouteResource:
 
     def generate_route(self, request):
         body = request.get_json()
-        if not body:
-            return {"error": "bad request", "description": "missing body"}
+        format_validation = self.__validate_request_format(body)
+        print("format")
+        print(format_validation.status)
+        if format_validation.status != "200 OK":
+            return format_validation
+
+        field_validation = self.__validate_request_fields(body)
+        print("field")
+        print(field_validation.status)
+        if field_validation.status != "200 OK":
+            return field_validation
 
         starting_point: dict = body["startingPoint"]
         restaurant_types: List[str] = body["type"]
@@ -58,3 +71,64 @@ class RouteResource:
             features.append(restaurant)
 
         return {"type": "FeatureCollection", "features": features}
+
+    def __validate_request_format(self, body):
+        error_message = ""
+        if not body:
+            error_message += "empty body\n"
+
+        if "length" not in body:
+            error_message += "body must contain field 'length'\n"
+
+        if "type" not in body:
+            error_message += "body must contain field 'type'\n"
+
+        if "startingPoint" not in body:
+            error_message += "body must contain field 'startingPoint'\n"
+
+        if "numberOfStops" not in body:
+            error_message += "body must contain field 'numberOfStops'\n"
+
+        if not isinstance(body["length"], int):
+            error_message += "''length' must be an integer\n"
+
+        for desired_type in body["type"]:
+            if not isinstance(desired_type, str):
+                error_message += "'type' must be an array of strings\n"
+
+        if not isinstance(body["numberOfStops"], int):
+            error_message += "''numberOfStops' must be an integer\n"
+
+        if not isinstance(body["startingPoint"], dict):
+            error_message += "''startingPoint' must be a geojson object\n"
+
+        schema = {
+            "type": "object",
+            "properties": {"type": {"type": "array"}},
+        }
+
+        try:
+            validate(body, schema)
+        except Exception:
+            error_message += "'type' must be an array of strings\n"
+
+        if error_message == "":
+            return make_response("", 200)
+
+        return make_response(error_message, 400)
+
+    def __validate_request_fields(self, body):
+        error_message = ""
+        if body["length"] <= 0:
+            error_message += "invalid value for 'length'"
+
+        valid_types = self.__restaurant_repository.get_restaurant_types()
+        for type in body["type"]:
+            if type not in valid_types:
+                error_message += "'type' contains invalid types"
+                break
+
+        if error_message == "":
+            return make_response("", 200)
+
+        return make_response(error_message, 404)
